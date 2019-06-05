33b1f18d8fc677e509c45aa670df1eab6772a7c2
Thomas Albrighton <thomasalbrighton@gmail.com>
2019-06-05 15:46:10 +0930

    Dave's notification

--- bin/deploy.js
+++ bin/deploy.js
@@ -51,7 +51,7 @@ function deploy() {
       "options": {"awslogs-group": "/ecs/rowan", "awslogs-region": "ap-southeast-2", "awslogs-stream-prefix": "ecs"}
     },
     "environment": env,
-    "memory": 256,
+    "memory": 200,
     "image": `${process.env.DOCKER_REPO}:${sha}`,
     "name": "rowan"
   }]
